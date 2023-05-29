d516abefa05001daa318ec5437a0cc2bcff79384
walkxcode <walkxnl@gmail.com>
2023-05-29 19:16:22 +0200

    :mag: Improves SEO

--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -11,25 +11,30 @@ const render = (pageContext: PageContext) => {
   const pageHtml = renderToString(() => <Page {...pageProps} />);
 
   const { documentProps } = pageContext.exports;
-  const title = (documentProps && documentProps.title) || "Walkx";
-  const description = (documentProps && documentProps.description) || "Walkx, tech enthusiast.";
+  const title = (documentProps && documentProps.title) || "Walkx - Home";
+  const description = (documentProps && documentProps.description) || "Hey, I'm Walkx. A tech enthusiast from the Netherlands.";
 
   return escapeInject`<!DOCTYPE html>
     <html lang="en">
       <head>
+        <title>${title}</title>
         <meta charset="UTF-8" />
-        <meta name="author" content="Walkx" />
-        <meta property="og:title" content="Walkx" />
+        <meta name="title" content="${title}" />
+        <meta name="description" content="${description}" />
+        <meta name="keywords" content="Walkx, walkxcode, walkx.fyi, walkx.org, walkyltd.xyz, Walker21390, Walker, Walkx Shoes, Coding, Tech Enthusiast, GitHub, Dashboard Icons, Uw Computerstudent, Digigem, Archiveteam ARM, Archiveteam, Winnie the Pooh, Tech Walkx, Walkx Organization, Coding Shoes, Tech Coding, Coding Enthusiast, Software Development, Web Development, Computer Science, Programming, Developer, GitHub Repository, Technology, Software Engineering" />
+        <meta name="author" content="Bjorn Lammers" />
+        <meta property="og:title" content="{title}" />
         <meta property="og:image" content="https://walkx.fyi/avatar.png" />
-        <meta property="description" content="${description}" />
         <meta property="og:description" content="${description}" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:creator" content="@walkxtweet" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="7 days" />
         <link rel="canonical" href="https://walkx.fyi" />
         <link rel="icon" href="/favicon.ico" />
         <link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&f[]=cabinet-grotesk@1&display=swap" rel="stylesheet">
-        <title>${title}</title>
         ${dangerouslySkipEscape(generateHydrationScript())}
       </head>
       <body>
